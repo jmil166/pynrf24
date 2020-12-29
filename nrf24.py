52928c2f3f85d894b3c2334c5f0db739341097d7
Jeremy Miller <millerj@jeremys-mbp.home>
2020-12-29 17:01:13 +0000

    d

--- nrf24.py
+++ nrf24.py
@@ -800,7 +800,7 @@ class NRF24:
             config |= NRF24.CRCO
             self.crc_length = 2
 
-        print("Config: "+config)
+        print("Config: "+ str(config))
         self.write_register(NRF24.CONFIG, int(config))
 
     def getCRCLength(self):
