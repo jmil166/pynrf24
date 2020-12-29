4b04bb2f7b8b5ad0e3a9fb0827dfb7ca445c95a8
Jeremy Miller <millerj@jeremys-mbp.home>
2020-12-29 17:37:25 +0000

    s

--- nrf24.py
+++ nrf24.py
@@ -800,7 +800,7 @@ class NRF24:
             config |= NRF24.CRCO
             self.crc_length = 2
 
-        self.write_register(NRF24.CONFIG, bin(config))
+        self.write_register(NRF24.CONFIG, config)
 
     def getCRCLength(self):
         result = NRF24.CRC_DISABLED
